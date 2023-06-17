d4310461091cd312d93bf1eddd3c31badd75d72c
AadityaNayak <aadityanayak7@gmail.com>
2023-06-18 03:24:46 +0530

    Pushed to github

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Table from './Table';
+import "./index.css";
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>User Table</h1>
+      <a href="https://github.com/AadityaNayak">GitHub</a>
+      <Table />
+    </div>
+  );
+};
+
+export default App;
